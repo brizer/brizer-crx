24de2956ad9afc38ccdb80e65765ff0eafd598f6
刘放 <362512489@qq.com>
2019-07-19 19:03:59 +0800

    chore: init build config

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = env => ({
+  entry: { 'ui/popup/popup': "./src/ui/popup/index.tsx" },
+  mode: process.env.NODE_ENV,
+  output: {
+    path: path.resolve(__dirname, "./dist/"),
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx", "json"]
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /(node_modules)/,
+        use:[
+            'ts-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './ui/popup/index.html',
+      template: path.resolve("./src/ui/popup/index.html")
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyPlugin([{ from: "./src/manifest.json", to: "./" },{from:"./src/icons",to:"./icons"}])
+  ]
+});
