6cdaac7c5ebfa4c9ae1173f740ec4558f8ed23bd
刘放 <362512489@qq.com>
2019-07-22 19:21:24 +0800

    refactor: edit column

--- src/ui/popup/components/url/Url.tsx
+++ src/ui/popup/components/url/Url.tsx
@@ -4,7 +4,7 @@ import { Table, Form, Input } from "antd";
 const UrlContext = React.createContext(null);
 
 const UrlRow = ({ form, index, ...props }) => {
-    console.log(form)
+  console.log(form);
   return (
     <UrlContext.Provider value={form}>
       <tr {...props} />
@@ -14,86 +14,90 @@ const UrlRow = ({ form, index, ...props }) => {
 
 const UrlFormRow = Form.create()(UrlRow);
 
-
 class EditableCell extends React.Component {
-    state = {
-      editing: false,
-    };
-  
-    toggleEdit = () => {
-      const editing = !this.state.editing;
-      this.setState({ editing }, () => {
-        if (editing) {
-          this.input.focus();
-        }
-      });
-    };
-  
-    save = e => {
-      const { record, handleSave }:any = this.props;
-      this.form.validateFields((error, values) => {
-        if (error && error[e.currentTarget.id]) {
-          return;
-        }
-        this.toggleEdit();
-        handleSave({ ...record, ...values });
-      });
-    };
-  
-    renderCell = form => {
-      this.form = form;
-      const { children, dataIndex, record, title }:any = this.props;
-      const { editing } = this.state;
-      return editing ? (
-        <Form.Item style={{ margin: 0 }}>
-          {form.getFieldDecorator(dataIndex, {
-            rules: [
-              {
-                required: true,
-                message: `${title} is required.`,
-              },
-            ],
-            initialValue: record[dataIndex],
-          })(<Input ref={node => (this.input = node)} onPressEnter={this.save} onBlur={this.save} />)}
-        </Form.Item>
-      ) : (
-        <div
-          className="editable-cell-value-wrap"
-          style={{ paddingRight: 24 }}
-          onClick={this.toggleEdit}
-        >
-          {children}
-        </div>
-      );
-    };
-    input: any;
-    form: any;
-  
-    render() {
-      const {
-        editable,
-        dataIndex,
-        title,
-        record,
-        index,
-        handleSave,
-        children,
-        ...restProps
-      }:any = this.props;
-      return (
-        <td {...restProps}>
-          {editable ? (
-            <UrlContext.Consumer>{this.renderCell}</UrlContext.Consumer>
-          ) : (
-            children
-          )}
-        </td>
-      );
-    }
+  state = {
+    editing: false
+  };
+
+  toggleEdit = () => {
+    const editing = !this.state.editing;
+    this.setState({ editing }, () => {
+      if (editing) {
+        this.input.focus();
+      }
+    });
+  };
+
+  save = e => {
+    const { record, handleSave }: any = this.props;
+    this.form.validateFields((error, values) => {
+      if (error && error[e.currentTarget.id]) {
+        return;
+      }
+      this.toggleEdit();
+      handleSave({ ...record, ...values });
+    });
+  };
+
+  renderCell = form => {
+    this.form = form;
+    const { children, dataIndex, record, title }: any = this.props;
+    const { editing } = this.state;
+    return editing ? (
+      <Form.Item style={{ margin: 0 }}>
+        {form.getFieldDecorator(dataIndex, {
+          rules: [
+            {
+              required: true,
+              message: `${title} is required.`
+            }
+          ],
+          initialValue: record[dataIndex]
+        })(
+          <Input
+            ref={node => (this.input = node)}
+            onPressEnter={this.save}
+            onBlur={this.save}
+          />
+        )}
+      </Form.Item>
+    ) : (
+      <div
+        className="editable-cell-value-wrap"
+        style={{ paddingRight: 24 }}
+        onClick={this.toggleEdit}
+      >
+        {children}
+      </div>
+    );
+  };
+  input: any;
+  form: any;
+
+  render() {
+    const {
+      editable,
+      dataIndex,
+      title,
+      record,
+      index,
+      handleSave,
+      children,
+      ...restProps
+    }: any = this.props;
+    return (
+      <td {...restProps}>
+        {editable ? (
+          <UrlContext.Consumer>{this.renderCell}</UrlContext.Consumer>
+        ) : (
+          children
+        )}
+      </td>
+    );
   }
+}
 
 const UrlCell = props => {
-
   const [state, setState] = useState({
     editing: false
   });
@@ -175,50 +179,49 @@ const UrlCell = props => {
 };
 
 const Url = () => {
-  const [state, setState] = useState<any>({
-    dataSource: [
-      {
-        key: 0,
-        str: "localhost:8080",
-        toStr: "http://www.baidu.com"
-      },
-      {
-        key: 1,
-        str: "localhost:8081",
-        toStr: "http://www.google.com"
-      }
-    ],
-    columns: [
-      {
-        title: "替换项",
-        dataIndex: "str",
-        width: "40%",
-        editable: true
-      },
-      {
-        title: "被替换项",
-        dataIndex: "toStr",
-        width: "40%",
-        editable: true
-      },
-      {
-        title: "操作",
-        dataIndex: "operation",
-        render: (text, record) => <a href="javascript:'">跳转</a>
-      }
-    ]
-  });
+  const [dataSource, setDataSource] = useState([
+    {
+      key: 0,
+      str: "localhost:8080",
+      toStr: "http://www.baidu.com"
+    },
+    {
+      key: 1,
+      str: "localhost:8081",
+      toStr: "http://www.google.com"
+    }
+  ]);
+
+  let [columns, setColumns] = useState([
+    {
+      title: "替换项",
+      dataIndex: "str",
+      width: "40%",
+      editable: true
+    },
+    {
+      title: "被替换项",
+      dataIndex: "toStr",
+      width: "40%",
+      editable: true
+    },
+    {
+      title: "操作",
+      dataIndex: "operation",
+      render: (text, record) => <a href="javascript:'">跳转</a>
+    }
+  ]);
 
   const components = {
     body: {
       row: UrlFormRow,
-    //   cell: UrlCell
+      //   cell: UrlCell
       cell: EditableCell
     }
   };
 
-  const columns = state.columns.map(col => {
-    if (!col.editabble) {
+  columns = columns.map(col => {
+    if (!col.editable) {
       return col;
     }
     return {
@@ -227,21 +230,21 @@ const Url = () => {
         record,
         editable: col.editable,
         dataIndex: col.dataIndex,
-        title: col.titie,
+        title: col.title,
         handleSave: handleSave
       })
     };
   });
 
   function handleSave(row) {
-    const newData = [...state.dataSource];
+    const newData = [...dataSource];
     const index = newData.findIndex(item => row.key === item.key);
     const item = newData[index];
     newData.splice(index, 1, {
       ...item,
       ...row
     });
-    setState({ dataSource: newData });
+    setDataSource(newData);
   }
 
   return (
@@ -250,7 +253,7 @@ const Url = () => {
         components={components}
         rowClassName={() => "editable-row"}
         bordered
-        dataSource={state.dataSource}
+        dataSource={dataSource}
         columns={columns}
         pagination={false}
       />
