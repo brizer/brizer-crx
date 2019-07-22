eccdada5110843d9fb9b132b346cb2b401355fcd
刘放 <362512489@qq.com>
2019-07-22 20:24:14 +0800

    refactor: handle delete

--- src/ui/popup/components/url/Url.tsx
+++ src/ui/popup/components/url/Url.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useContext } from "react";
-import { Table, Form, Input } from "antd";
+import React, { useState, useContext, useRef } from "react";
+import { Table, Form, Input, Button } from "antd";
 
 const UrlContext = React.createContext(null);
 
 const UrlRow = ({ form, index, ...props }) => {
-  console.log(form);
   return (
     <UrlContext.Provider value={form}>
       <tr {...props} />
@@ -14,7 +13,7 @@ const UrlRow = ({ form, index, ...props }) => {
 
 const UrlFormRow = Form.create()(UrlRow);
 
-class EditableCell extends React.Component {
+class UrlCell extends React.Component {
   state = {
     editing: false
   };
@@ -97,87 +96,6 @@ class EditableCell extends React.Component {
   }
 }
 
-const UrlCell = props => {
-  const [state, setState] = useState({
-    editing: false
-  });
-  let form;
-  let input;
-
-  const {
-    editable,
-    dataIndex,
-    title,
-    record,
-    index,
-    handleSave,
-    children,
-    ...restProps
-  } = props;
-
-  function toggleEdit() {
-    const editing = !state.editing;
-    if (editing) {
-      setState({
-        editing: input.focus()
-      });
-    }
-  }
-
-  function save(e) {
-    const { record, handleSave } = props;
-    form.validateFields((error, values) => {
-      if (error && error[e.currentTarget.id]) {
-        return;
-      }
-      toggleEdit();
-      handleSave({ ...record, ...values });
-    });
-  }
-
-  const renderCell = renderForm => {
-    form = renderForm;
-    const { children, dataIndex, record, title } = props;
-    const editing = state.editing;
-    return editing ? (
-      <Form.Item style={{ margin: 0 }}>
-        {renderForm.getFieldDecorator(dataIndex, {
-          rules: [
-            {
-              required: true,
-              message: `${title} is required.`
-            }
-          ],
-          initialValue: record[dataIndex]
-        })(
-          <Input
-            ref={node => (input = node)}
-            onPressEnter={save}
-            onBlur={save}
-          />
-        )}
-      </Form.Item>
-    ) : (
-      <div
-        className="editable-cell-value-wrap"
-        style={{ paddingRight: 24 }}
-        onClick={toggleEdit}
-      >
-        {children}
-      </div>
-    );
-  };
-  return (
-    <td {...restProps}>
-      {editable ? (
-        <UrlContext.Consumer>{renderCell}</UrlContext.Consumer>
-      ) : (
-        children
-      )}
-    </td>
-  );
-};
-
 const Url = () => {
   const [dataSource, setDataSource] = useState([
     {
@@ -208,15 +126,20 @@ const Url = () => {
     {
       title: "操作",
       dataIndex: "operation",
-      render: (text, record) => <a href="javascript:'">跳转</a>
+      render: (text, record) => (
+        <div>
+          <a href="javascript:;">跳转</a>
+          <a href="javascript:;" onClick={()=>handleDelete(record.key)}>删除</a>
+        </div>
+      )
     }
   ]);
 
   const components = {
     body: {
       row: UrlFormRow,
-      //   cell: UrlCell
-      cell: EditableCell
+      cell: UrlCell
+      //   cell: EditableCell
     }
   };
 
@@ -247,8 +170,24 @@ const Url = () => {
     setDataSource(newData);
   }
 
+  function handleAdd() {
+    const newData = {
+      key: +new Date(),
+      str: "被替换的字符串",
+      toStr: "替换成的字符串"
+    };
+    setDataSource([...dataSource, newData]);
+  }
+
+  function handleDelete(key:number){
+      setDataSource(dataSource.filter(item=>item.key!==key))
+  }
+
   return (
     <div>
+      <Button onClick={handleAdd} type="primary" style={{ marginBottom: 10 }}>
+        新增
+      </Button>
       <Table
         components={components}
         rowClassName={() => "editable-row"}
