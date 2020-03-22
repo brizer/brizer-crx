92e5a6a0e6e3cd79193c062c64ede757b8d9090d
刘放 <362512489@qq.com>
2020-03-22 13:40:28 +0800

    feat: add codemirror for code editor

--- src/ui/newtab/components/CodeEditor.tsx
+++ src/ui/newtab/components/CodeEditor.tsx
@@ -1,14 +1,47 @@
-import React, { FunctionComponent } from "react";
+require('codemirror/lib/codemirror.css');
+require('codemirror/theme/material.css');
+require('codemirror/theme/neat.css');
+require('codemirror/mode/xml/xml.js');
+require('codemirror/mode/javascript/javascript.js');
+
+import React, { FunctionComponent, useState } from "react";
+import { Button } from "antd";
+
+import { UnControlled as CodeMirror } from "react-codemirror2";
+import styles from "./CodeEditor.less";
+import "./CodeEditor.css";
 
 
 interface ICodeEditorProps {
-  
+  onRun: Function;
 }
 
 const CodeEditor: FunctionComponent<any> = (props: ICodeEditorProps) => {
+  const { onRun } = props;
+  const [jsCode, setJsCode] = useState("");
+  function run() {
+    onRun(jsCode);
+  }
   return (
-    <div>
-        1234
+    <div style={{ textAlign: "left" }}>
+      <h3>javascript</h3>
+      <Button onClick={run} type="primary">
+        Run
+      </Button>
+      <CodeMirror
+        className={styles.panel}
+        value=""
+        options={{
+          lineNumbers: true,
+          mode: "javascript",
+          indentUnit: 4,
+          indentWithTabs: true,
+          theme: "base16-dark"
+        }}
+        onChange={(editor, data, value) => {
+          setJsCode(value);
+        }}
+      />
     </div>
   );
 };
--- src/ui/newtab/layout/Editor.tsx
+++ src/ui/newtab/layout/Editor.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
+import { before } from "@tomato-js/function";
 import CodeEditor from "../components/CodeEditor";
 
 interface IEditorProps {
@@ -6,10 +7,23 @@ interface IEditorProps {
 }
 
 const Editor: FunctionComponent<any> = (props: IEditorProps) => {
+  const [output,setOutput] = useState('');
+  function onRun(jsCode:string){
+    eval(jsCode);
+  }
+  (window as any).log = before(console.log,(v)=>{
+    const { args } = v;
+    const value = args[0];
+    setOutput(value);
+  })
   return (
-    <div>
+    <div style={{textAlign:"left"}}>
       <h1>Editor</h1>
-      <CodeEditor />
+      <CodeEditor onRun={onRun}/>
+      <h2>console output:</h2>
+      <p>
+        {output}
+      </p>
     </div>
   );
 };
